fe2242c19b2ffe7d8331a64441abdbdfe0d68b79
fadhil0523 <fadhil0523@students.abudzar.sch.id>
2026-02-06 10:58:08 +0700

    first commit

---
+++ latihan-operator-math/script.js
@@ -0,0 +1,35 @@
+//latihan 1
+
+let layanganFuad = 5;
+let colorOne = 'red';
+let colorTwo = 'blue';
+let colorThree = 'green';
+let colorFour = 'yellow';
+let colorFive = 'purple';
+
+let masukGot = colorThree;
+let dicuri = colorFive;
+
+let updateLayanganFuad = layanganFuad - 2;
+
+console.log('jumlah layangan fuad adalah' + ' ' + layanganFuad);
+console.log('layangan yg rusak' + ' ' + masukGot +  ' ' + dicuri);
+console.log('sisa layangan' + ' ' + updateLayanganFuad);
+
+
+//latihan 2
+
+let iceCreamRaffa = 3;
+let iceCreamOne = 'matcha';
+let iceCreamTwo = 'chocolate';
+let iceCreamThree = 'vanilla';
+
+let updateIceCreamRaffa = iceCreamRaffa - 1;
+
+console.log('jumlah ice cream raffa ' + iceCreamRaffa);
+console.log('rasa ice cream yg kedua ' + iceCreamTwo);
+console.log('rasa ice cream yg ketiga ' + iceCreamThree);
+console.log('ice cream yg diberikan ke anhar ' + iceCreamTwo);
+console.log('total ice cream raffa sekarang ' + updateIceCreamRaffa);
+
+
