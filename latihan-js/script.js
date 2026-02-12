3c9dd2c5d655f7d56f67934bef7f803b80b83b9a
fadhil0523 <fadhil0523@students.abudzar.sch.id>
2026-02-12 10:38:55 +0700

    tugas-31

---
+++ latihan-js/script.js
@@ -0,0 +1,43 @@
+//soal 1
+
+let apel = 10000;
+let jeruk = 12000;
+let susu = 15000;
+
+let totalBiaya = apel + jeruk + susu;
+console.log('total biaya yang harus dikeluarkan budi' + ' ' + totalBiaya);
+
+
+
+//soal 2
+
+let kelerengRina = 50;
+let diberikan = 20;
+
+let totalAwal = kelerengRina - diberikan;
+let totalAkhir = totalAwal * 3;
+console.log('total kelereng rina sekarang' + ' ' + totalAkhir);
+console.log(totalAkhir**2);
+
+
+
+//soal 3
+let buku = 3000;
+let pensil = 1500;
+let penghapus = 2000;
+
+let totalBiayaSiti = buku + pensil + penghapus;
+console.log('total biaya yang harus di keluarkan siti' + ' ' + totalBiayaSiti);
+
+ 
+
+//soal 4
+let manggaDoni = 40;
+let manggalDiJual = 15;
+
+let manggaSisa =  manggaDoni - manggalDiJual;
+let manggaPanen = manggaSisa * 2;
+console.log('mangga doni sekarang ' + manggaPanen);
+console.log(manggaPanen**2);
+
+ 
---
+++ tugas-31/script.js
@@ -0,0 +1,6 @@
+let dressSize = 45;
+console.log(String(dressSize));
+console.log(Number(dressSize));
+
+let isLight = true
+console.log(String(isLight));
