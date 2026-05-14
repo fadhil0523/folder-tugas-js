de62d0c26088b962613f20a02ddb91ed850c67ca
fadhil0523 <fadhil0523@students.abudzar.sch.id>
2026-05-14 10:06:13 +0700

    tugas-38

---
+++ tugas-38/script.js
@@ -0,0 +1,15 @@
+//soal 1
+let apel = null;
+let mangga = '';
+let jeruk = 'jeruk ada';
+
+console.log(apel || mangga || jeruk ); //jeruk ada
+
+//soal 2
+let teh = null;
+let kopi = undefined;
+let susu = 10;
+
+console.log(teh ?? kopi ?? susu); //10
+
+
